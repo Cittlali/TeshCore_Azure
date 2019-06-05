f381343c0bda72d39a10406d8cebe7c880df5d58
CIT M <chakly_25@live.com.mx>
2019-06-05 16:50:37 -0500

    Azure

---
+++ teshCore/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using teshCore.Models;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace teshCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IConfiguration Configuration { get; }
+        public HomeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+        }
+        public IActionResult Index()
+
+        {
+
+            return View();
+        }
+        public IActionResult Create2()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create2(Alumnos alumno) { 
+            if(ModelState.IsValid)
+            {
+            string connectionString = Configuration ["ConnectionStrings:SQLConnection"];
+           using (SqlConnection connection= new SqlConnection(connectionString))
+    {
+                    string sql = $"Insert Into Alumnos(nombreAlumno,apellidoAlumno,calificacionAlumno,fechaNacimientoAlumno) Values ('{alumno.nombreAlumno}','{alumno.apellidoAlumno}','{alumno.calificacionAlumno}','{alumno.fechaNacimientoAlumno}')";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.CommandType = CommandType.Text;
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                        connection.Dispose();
+                    }
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+                return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ teshCore/Models/Alumnos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace teshCore.Models
+{
+    public class Alumnos
+    {
+        public int Id { get; set; }
+        public string nombreAlumno{ get; set; }
+        public string apellidoAlumno { get; set; }
+        public int calificacionAlumno { get; set; }
+        public DateTime fechaNacimientoAlumno { get; set; } 
+
+    }
+}
